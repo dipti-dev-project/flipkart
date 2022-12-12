03b88d2ca8b3acba36a8f4eceebab17966dc1930
diptiranjan <drmalla1@gmail.com>
2022-12-12 23:46:52 +0530

    updating for test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ This file contains the deployment script.
 I have updated something in the first line.
 I have updated manually.
 Update the code for test in branch.
+I am updating line this from devops branch.
+
 
