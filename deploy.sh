99806fa519bbdaa0ee733beb78d0c7a268a0e4d3
diptiranjan <drmalla1@gmail.com>
2022-12-12 23:40:46 +0530

    update for merging

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ This file contains the deployment script.
 I have updated something in the first line.
 I have updated manually.
 Update the code for test in branch.
+I am updating this line from master branch.
 
